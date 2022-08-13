6826653d83dc987adf1e6fcf8cb3e824fdf323e0
Abdulrahmankhaled11 <bodyelazar22@gmail.com>
2022-08-13 17:12:12 +0200

    add a commit

---
+++ task 1.py
@@ -0,0 +1,43 @@
+#● Create a Boolean variable named x
+x = true
+#● Create an integer variable named y
+y = 3
+#● Create a float variable named z
+z = 3.25
+#● Create a string variable names s
+s = 'Abdulrahman'
+#● Convert the int variable to float
+y = 2.5
+#● Create a list of numbers from 1 to 5
+L = [1,2,3,4,5]
+#● Create a tuple from 10 to 15
+T = (10,11,12,13,14,15)
+#● Convert the list to tuple
+L = (1,2,3,4,5)
+#● Create a dict of 3 values
+D = {1:'ahmed',2:'abdulrahman',3:'mahmoud'}
+#● Create a variable x with value of 10
+x = 10
+#● Increase x value by 15 using assignment operators
+x+=15
+#● Divide the x value by 5 using assignment operators
+x/=5
+#● Multiply the x value by 10 using assignment operator
+x*=10
+#● Get module of x on 3 using assignment operators
+x%=3
+#● Using python print the module of 11 to 4
+h = 11%4
+print(h)
+#● Print the exponent of 2,3
+k =2**3
+print(k)
+#● Divide 11 by 3 using python division
+11/3
+#● Check if 10 is bigger than 15 or not
+if 10>15 :
+    print('yes')
+else:
+    print(' x is smaller than 15')
+#● write s simple ternary operator
+print('yes') if 10>15 else print(' x is smaller than 15')
